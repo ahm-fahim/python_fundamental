66076f5ad933b4a32535df727005dafb656b2009
mdFahimMorshed <contactahmfahim@gmail.com>
2022-02-12 22:45:32 +0600

    turtle

---
+++ Fundamental of Python/Turtle intro/program2.py
@@ -0,0 +1,27 @@
+import turtle 
+
+turtle.shape("turtle")
+turtle.speed(10)
+
+turtle.forward(400)
+turtle.left(90)
+turtle.forward(400)
+turtle.left(90)
+turtle.forward(800)
+turtle.left(90)
+turtle.forward(400)
+turtle.left(90)
+turtle.forward(400)
+turtle.left(45)
+turtle.forward(200)
+turtle.left(90)
+turtle.forward(200)
+turtle.left(90)
+turtle.forward(200)
+turtle.left(90)
+turtle.forward(200)
+turtle.left(90)
+
+
+
+turtle.exitonclick()
