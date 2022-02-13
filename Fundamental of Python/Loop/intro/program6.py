8a5676e50e501974012e408504d4bf2e65c98610
mdFahimMorshed <contactahmfahim@gmail.com>
2022-02-13 16:02:12 +0600

    loop

---
+++ Fundamental of Python/Loop/intro/program6.py
@@ -0,0 +1,7 @@
+result = 0
+
+for i in range(3, 5):
+    i += 1
+    result += i
+
+print(result)
---
+++ Fundamental of Python/Loop/intro/program7.py
@@ -0,0 +1,10 @@
+import re
+
+
+result = 0
+
+for i in range(2, 20, 3):
+    i += 1
+    result += i
+
+print(result)
