70921514badc331845925e62d7b4f4717a378fd7
mdFahimMorshed <contactahmfahim@gmail.com>
2022-02-16 11:36:39 +0600

    string

---
+++ Fundamental of Python/String/program1.py
@@ -0,0 +1,19 @@
+s = "fahim"
+print(len(s))
+s = ''
+print(len(s))
+s = "fahim's garden"
+print(s)
+s = 'fahim\'s diary'
+print(s)
+s = "Fahim"
+print(s[0])
+print(s[1])
+print(s[2])
+print(s[4])
+
+print("\nloop\n")
+for i in s:
+    print(i)
+
+
