4481c5cef3438dd9b112e54ceb1c4c51d3362405
mdFahimMorshed <contactahmfahim@gmail.com>
2022-02-16 12:17:19 +0600

    string replace

--- Fundamental of Python/String/program1.py
+++ Fundamental of Python/String/program1.py
@@ -17,3 +17,22 @@ for i in s:
     print(i)
 
 
+li = ['P','Y','T','H','O','N']
+print(li)
+li[0] = 'p'
+print(li)
+
+#but we can't change string item by indexing
+#for this reasons string is a immutable / non-mutable value
+
+first_name = "fahim"
+last_name = " morshed"
+
+full_name = first_name + last_name
+print(full_name)
+
+num1 = "10"
+num2 = "10"
+sum = num1 + num2
+print(sum)
+
