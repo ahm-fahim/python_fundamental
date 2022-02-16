05c6df01143a3935bd5f66c04720fb75ba6d8289
mdFahimMorshed <contactahmfahim@gmail.com>
2022-02-16 13:01:35 +0600

    string and method

---
+++ Fundamental of Python/PYTHON METHOD/find.py
@@ -0,0 +1,9 @@
+name = "pyhton"
+find_value = name.find('o')
+print(find_value)
+
+find_value = name.find('f')
+print(find_value)
+
+
+
---
+++ Fundamental of Python/PYTHON METHOD/replace.py
@@ -0,0 +1,5 @@
+old_version = "python 2.0"
+print("Old version:",old_version)
+
+new_version = old_version.replace("2.0", "3.10")
+print("New version:",new_version)
