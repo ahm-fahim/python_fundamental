387a0cd4ffd2f695a5103ee4802a002be158ce76
mdFahimMorshed <contactahmfahim@gmail.com>
2022-02-18 11:12:40 +0600

    list append method

---
+++ Fundamental of Python/PYTHON METHOD/append.py
@@ -0,0 +1,7 @@
+my_list = ["laptop","phone","python book"]
+print(my_list)
+my_list.append("C book")
+print(my_list)
+
+#list is mutbale . for  this reasons when we add a new value on 
+#the list , the main list will be changed
