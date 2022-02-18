5bb374eba3f083a1e06d81004768f00ff6b2dec0
mdFahimMorshed <contactahmfahim@gmail.com>
2022-02-18 11:25:17 +0600

    insert method

---
+++ Fundamental of Python/Data structure/list/insert.py
@@ -0,0 +1,7 @@
+todo_list = ["lunch","dinner"]
+print(todo_list)
+todo_list.insert(0,"break fast")
+               #(index,  item  )
+print(todo_list)
+todo_list.insert(2,"playing cricket")
+print(todo_list)
