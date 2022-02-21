23748555fe1fb226d849aabc7ff7635cb74ce08e
mdFahimMorshed <contactahmfahim@gmail.com>
2022-02-21 19:36:50 +0600

    list mathematical operation

---
+++ Fundamental of Python/Data structure/list/append2.py
@@ -0,0 +1,4 @@
+li = [] #empty list
+for item in range(10):
+    li.append(item)
+print(li)
---
+++ Fundamental of Python/Data structure/list/list_math_operation.py
@@ -0,0 +1,8 @@
+li1 = [10, 20, 30]
+li2 = [4, 3, 2]
+add_list = li1 + li2
+print(add_list)
+
+mult_list = li1 * 2``
+print(mult_list)
+
