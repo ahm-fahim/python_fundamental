25983f58ceec7de1a1bf9e07eb2d1fc70cf2a0d9
mdFahimMorshed <contactahmfahim@gmail.com>
2022-02-22 19:45:30 +0600

    dictionary

---
+++ Fundamental of Python/Data structure/dictionary/division.py
@@ -0,0 +1,17 @@
+bd_division_info = {}
+bd_division_info["Barisal"] = {"district": 6, "upazila": 39, "union":333}
+bd_division_info["Chittagong"] = {"district": 11, "upazila": 97, "union":336}
+bd_division_info["Dhaka"] = {"district": 13, "upazila": 93, "union":1833}
+bd_division_info["Khulna"] = {"district": 10, "upazila": 59, "union":270}
+bd_division_info["Mymensingh"] = {"district": 4, "upazila": 34, "union":350}
+
+print(bd_division_info.keys())
+
+for divission in bd_division_info:
+    # print(divission,bd_division_info[divission]["upazila"])
+    print(bd_division_info[divission])
+
+
+
+
+
