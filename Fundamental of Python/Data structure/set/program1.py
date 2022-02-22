076035c1c2bcdc41d0329e7dd94886f3e01de2ea
mdFahimMorshed <contactahmfahim@gmail.com>
2022-02-22 11:25:28 +0600

    set

---
+++ Fundamental of Python/Data structure/set/program1.py
@@ -0,0 +1,19 @@
+a = set() #empty set
+print(a) 
+print(type(a))
+
+items = {"pen", "laptop","cellphone"}
+print(items)
+print(type(items))
+
+#we can make a set from tuple and list
+
+li = [1,2,3,4]
+tpl = (2,4,6)
+
+new_set1 = set(li)
+print(new_set1)
+new_set2 = set(tpl)
+print(new_set2)
+
+
