cefed4d3f35d31b9fdf31a5ea4f0d1b9de479619
mdFahimMorshed <contactahmfahim@gmail.com>
2022-02-22 16:16:46 +0600

    dictionary

---
+++ Fundamental of Python/Data structure/dictionary/program2.py
@@ -0,0 +1,3 @@
+marks = {"DH2001": 76, "DH3001": 88, "DH4001": 85}
+print(marks)
+print(marks["DH2001"])
---
+++ Fundamental of Python/Data structure/dictionary/program3.py
@@ -0,0 +1,10 @@
+empty_dict = {}
+print(empty_dict)
+print(type(empty_dict))
+empty_dict[66656] = "software development"
+empty_dict[66657] = "android development"
+
+print(empty_dict)
+
+#dictionary is not mutable . so its value never changed after assign
+
