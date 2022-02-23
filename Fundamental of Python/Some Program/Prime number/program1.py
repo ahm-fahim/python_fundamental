5f4deebf542d84a7fdfee86144cd44f5c38695da
mdFahimMorshed <contactahmfahim@gmail.com>
2022-02-23 22:48:28 +0600

    prime number

---
+++ Fundamental of Python/Some Program/Prime number/program1.py
@@ -0,0 +1,20 @@
+def is_prime1(n):
+    if n < 2:
+        return False
+    prime = True
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, "is divisible by ",x)
+            prime = False
+    return prime
+
+while True:
+    number = int(input("Input(0 to exit):"))
+  
+    if number == 0:
+        break
+    prime = is_prime1(number)
+    if prime is True:
+        print(number, "is a prime number")
+    else:
+        print(number, "is not a prime number")
