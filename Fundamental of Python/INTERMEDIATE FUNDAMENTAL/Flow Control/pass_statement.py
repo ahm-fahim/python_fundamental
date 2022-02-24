f71c77ec274feeb1b54c7f6cb7a210b9aff8d481
mdFahimMorshed <contactahmfahim@gmail.com>
2022-02-24 16:41:20 +0600

    pass statement

---
+++ Fundamental of Python/INTERMEDIATE FUNDAMENTAL/Flow Control/pass_statement.py
@@ -0,0 +1,11 @@
+'''pass is just a placeholder for
+functionality to be added later.'''
+sequence = {'p', 'a', 's', 's'}
+for val in sequence:
+    pass
+
+def function(args):
+    pass
+
+class Example:
+    pass
