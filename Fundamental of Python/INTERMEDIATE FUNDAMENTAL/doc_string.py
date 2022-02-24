6f000d07d0d1cda5669822ec2c64d7d873d82ce9
mdFahimMorshed <contactahmfahim@gmail.com>
2022-02-24 15:24:04 +0600

    python literals

---
+++ Fundamental of Python/INTERMEDIATE FUNDAMENTAL/doc_string.py
@@ -0,0 +1,17 @@
+"""
+Docstrings in Python
+A docstring is short for documentation string.
+
+Python docstrings (documentation strings) are the string literals that appear right after the definition of a function, method, class, or module.
+
+Triple quotes are used while writing docstrings.
+"""
+def double(num):
+    """Function to double the value"""
+    return 2*num
+print(double.__doc__)
+
+def docStr(n):
+    """Doc string"""
+
+print(docStr.__doc__)
