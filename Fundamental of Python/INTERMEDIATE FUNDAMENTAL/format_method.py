6cd71dad5d6f49de78e28a0e4879746593759b9e
mdFahimMorshed <contactahmfahim@gmail.com>
2022-02-24 15:45:35 +0600

    format method

--- Fundamental of Python/INTERMEDIATE FUNDAMENTAL/format_method.py
+++ Fundamental of Python/INTERMEDIATE FUNDAMENTAL/format_method.py
@@ -1,2 +1,7 @@
 x = 5; y = 10
 print('The value of x is {} and y is {}'.format(x,y))
+
+print('Hello {name}, {greeting}'.format(greeting = 'Good morning', name = 'John'))
+
+x = 12.3456789
+print('The value of x is %3.2f' %x)
